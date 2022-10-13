5519e95548906218d4be4c2e6b8893ee6afdf536
junel <sacrojunel@gmail.com>
2022-10-13 19:13:12 +0800

    update: footer

---
+++ components/Home/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className="h-16 bg-slate-800 flex text-white ">
+            <ul className="flex items-center justify-items-center space-x-5">
+                <li>Home</li>
+                <li>Calculator</li>
+                <li>About Us</li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer
