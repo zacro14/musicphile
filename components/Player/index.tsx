25b39debf91c8f142a0333310c8bf12b963521c9
junel <sacrojunel@gmail.com>
2022-11-06 21:54:20 +0800

    chore: add the current active song in music player

--- components/Player/index.tsx
+++ components/Player/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { playpause } from 'redux/features/playerSlice'
+import { useAppDispatch, useAppSelector } from 'redux/store'
 import Control from './Control'
 import Player from './Player'
 import SeekStatus from './SeekStatus'
@@ -7,17 +9,41 @@ import Track from './Track'
 import Volume from './Volume'
 
 const MusicPlayer = () => {
+    const { isActive, isPlaying, activeSong } = useAppSelector(
+        (state) => state.player
+    )
+    const dispatch = useAppDispatch()
+
+    const handleplaypause = () => {
+        if (!isActive) return
+
+        if (isPlaying) {
+            dispatch(playpause(false))
+        } else {
+            dispatch(playpause(true))
+        }
+    }
     return (
         <>
             <div className="fixed h-20 w-full text-white bottom-0 bg-gray-900 z-10 border-t border-slate-300 border-opacity-10 py-3 px-5">
                 <SeekStatusMobile />
-                <div className="flex items-center gap-x-3">
-                    <Track />
-                    <SeekStatus />
-                    <Control />
-                    <Volume />
-                    <Player />
-                </div>
+                {activeSong && (
+                    <div className="flex items-center gap-x-3">
+                        <Track
+                            title={activeSong?.title}
+                            subtitle={activeSong?.subtitle}
+                            coverImage={activeSong?.image?.background}
+                        />
+
+                        <SeekStatus />
+                        <Control
+                            isPlaying={isPlaying}
+                            handlePlayPause={handleplaypause}
+                        />
+                        <Volume />
+                        <Player activeSong={activeSong} isPlaying={false} />
+                    </div>
+                )}
             </div>
         </>
     )
