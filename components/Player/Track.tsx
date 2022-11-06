488d45a09259312ac46121c98009bdd6bdca4c7f
junel <sacrojunel@gmail.com>
2022-11-06 21:51:50 +0800

    chore: change to dynamic values for music player current tract playing

--- components/Player/Track.tsx
+++ components/Player/Track.tsx
@@ -2,20 +2,26 @@ import Image from 'next/image'
 import React from 'react'
 import TrackImage from '@assets/image/200x200bb more_chart_2.webp'
 
-const Track = () => {
+interface ITrackProps {
+    title: string
+    subtitle: string
+    coverImage: any
+}
+const Track = (props: ITrackProps) => {
+    const { title, subtitle, coverImage } = props
     return (
         <div className="flex">
             <div className="hidden md:flex lg:flex relative h-14 w-14 mr-3">
                 <Image
                     className="rounded-lg"
-                    src={TrackImage}
-                    alt={'now playing'}
+                    src={coverImage}
+                    alt={title}
                     layout={'fill'}
                 />
             </div>
             <div className="flex flex-col">
-                <span className="font-bold">{'Im Good (Blue)'}</span>
-                <span className="text-sm text-slate-400">{'David Gueta'}</span>
+                <span className="font-bold">{title}</span>
+                <span className="text-sm text-slate-400">{subtitle}</span>
             </div>
         </div>
     )
