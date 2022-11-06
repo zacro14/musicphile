e1f01ca57d6c3955f34a577d440482d4ce97fdec
junel <sacrojunel@gmail.com>
2022-11-06 21:53:07 +0800

    feat ( player component ): add the src for audio src

--- components/Player/Player.tsx
+++ components/Player/Player.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { IActiveSongProps } from 'redux/features/playerSlice'
+interface PlayerProps {
+    activeSong: IActiveSongProps
+    isPlaying: boolean
+}
 
-const Player = () => {
-    return <audio />
+const Player = (props: PlayerProps) => {
+    const { activeSong } = props
+    return <audio src={activeSong?.data?.hub?.actions[1]?.uri} />
 }
 
 export default Player
