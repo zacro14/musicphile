c33f16ad54cf217bf1b22121deff707771d0fbcb
junel <sacrojunel@gmail.com>
2022-11-08 19:23:37 +0800

    feat(banner): add the a card on the banner for global chart

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -1,9 +1,61 @@
+import Image from 'next/image'
 import React from 'react'
 
-const Banner = () => {
+interface Banner {
+    images: any
+    title: string
+}
+interface BannerProps {
+    banner: any[]
+}
+const Banner = (props: BannerProps) => {
+    const { banner } = props
+
     return (
-        <section className="h-96 bg-slate-100 w-full grid place-items-center">
-            <h1 className="font-bold text-3xl text-center">Top Global Songs</h1>
+        <section className=" h-72 md:h-96 bg-slate-100 w-full overflow-hidden ">
+            <div className="relative mx-auto max-w-7xl p-5 flex items-center h-full">
+                <div className="absolute top-0 left-0 right-0 bottom-0">
+                    <ul className="absolute -top-60 right-40 -rotate-45">
+                        <li className="absolute shadow-2xl md:w-40 md:h-40 top-44 right-44 rounded-lg">
+                            <Image
+                                className="rounded-lg"
+                                src={banner[0].images.background}
+                                alt={banner[0].title}
+                                layout={'fill'}
+                            />
+                        </li>
+                        <li className="absolute shadow-2xl md:w-40 md:h-40 top-0 right-44">
+                            <Image
+                                className="rounded-lg"
+                                src={banner[1].images.background}
+                                alt={banner[1].title}
+                                layout={'fill'}
+                            />
+                        </li>
+                        <li className="absolute shadow-2xl md:w-40 md:h-40 top-44 right-0">
+                            <Image
+                                className="rounded-lg"
+                                src={banner[2].images.background}
+                                alt={banner[2].title}
+                                layout={'fill'}
+                            />
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="flex md:flex-row lg:flex-row items-center space-x-5">
+                    <div className=" w-40 h-40 md:w-80 md:h-80 bg-slate-200 flex items-center justify-evenly flex-col ">
+                        <h2 className="text-2xl md:text-3xl font-bold">
+                            Top 50
+                        </h2>
+                        <hr className="border border-slate-400 w-2/5"></hr>
+                        <span>Global</span>
+                    </div>
+                    <h1 className="font-bold text-3xl md:text-5xl lg:text-6xl text-center">
+                        Top Global Songs
+                    </h1>
+                </div>
+            </div>
         </section>
     )
 }
--- pages/charts/index.tsx
+++ pages/charts/index.tsx
@@ -3,11 +3,13 @@ import Banner from '@components/Banner/Banner'
 import SonglistItem from '@components/SonglistItem'
 import Songs from '@assets/data/getWorldChart.json'
 import Seo from '@components/Seo/Seo'
+
 const Charts = () => {
+    console.log(Songs.slice(0, 2))
     return (
         <>
             <Seo title="Music Just For You" description="discover music" />
-            <Banner />
+            <Banner banner={Songs.slice(0, 3)} />
             <section className="min-h-screen mx-auto max-w-7xl">
                 <section className="pt-5 text-white px-5">
                     {Songs.map((song: any, i: number) => (
